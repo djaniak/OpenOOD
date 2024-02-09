ed9990a89ad1312751cceab3e805a2dfab7d0445
djaniak <denis.janiak7@gmail.com>
2024-02-09 14:02:01 +0100

    fix: rkldivsim accordingly

--- openood/postprocessors/rkldivsim_postprocessor.py
+++ openood/postprocessors/rkldivsim_postprocessor.py
@@ -6,13 +6,14 @@ import torch.nn as nn
 from torch.distributions import Independent, Normal, kl_divergence
 from tqdm import tqdm
 
-from .base_postprocessor import BasePostprocessor
+from openood.postprocessors.base_postprocessor import BasePostprocessor
 
 
-class RKlDivSimPostprocessor(BasePostprocessor):
+class KlDivSimPostprocessor(BasePostprocessor):
     def __init__(self, config):
         self.config = config
         self.setup_flag = False
+        self.APS_mode = False
 
     def setup(self, net: nn.Module, id_loader_dict, ood_loader_dict):
         if not self.setup_flag:
@@ -26,12 +27,11 @@ class RKlDivSimPostprocessor(BasePostprocessor):
                         id_loader_dict["train"], desc="Setup: ", position=0, leave=True
                 ):
                     if isinstance(batch["data"], list):
-                        x1, x2, x3 = batch["data"]
-                        data = (x1.cuda(), x2.cuda(), x3.cuda())
+                        _, _, x = batch["data"]
                     else:
-                        data = batch["data"].cuda()
+                        x = batch["data"]
                     labels = batch["label"]
-                    logits, (mus, logvars) = net(data, return_dist=True)
+                    logits, (mus, logvars) = net(x.cuda(), return_dist=True)
                     all_mus.append(mus)
                     all_logvars.append(logvars)
                     all_labels.append(deepcopy(labels))
@@ -46,20 +46,23 @@ class RKlDivSimPostprocessor(BasePostprocessor):
             train_acc = all_preds.eq(all_labels).float().mean()
             print(f" Train acc: {train_acc:.2%}")
 
-            self.train_q = Independent(Normal(all_mus, all_logvars), 1)
+            self.train_q = Independent(Normal(all_mus, torch.exp(0.5 * all_logvars)), 1)
             self.setup_flag = True
         else:
             pass
 
     @torch.no_grad()
     def postprocess(self, net: nn.Module, data: Any):
-        logits, (mus, logvars) = net(data, return_dist=True)
+        x1, x2, x3 = data
+        logits, (mus, logvars) = net(x3, return_dist=True)
         pred = logits.argmax(1)
 
         conf = []
-        for mu, logvar in tqdm(zip(mus, logvars)):
-            q = Independent(Normal(mu, logvar), 1)
-            min_kldiv = torch.min(kl_divergence(q, self.train_q))
+        for mu, logvar in zip(mus, logvars):
+            q = Independent(Normal(mu, torch.exp(0.5 * logvar)), 1)
+            kl_divs = kl_divergence(q, self.train_q)
+            kl_divs = torch.nan_to_num(kl_divs, nan=torch.finfo(torch.float16).max)
+            min_kldiv = torch.min(kl_divs)
             conf.append(min_kldiv)
         conf = torch.hstack(conf).cpu()
 
