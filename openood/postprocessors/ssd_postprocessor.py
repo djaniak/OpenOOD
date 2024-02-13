efc068769cfee5b2d863019f7b176a5f5687a151
djaniak <denis.janiak7@gmail.com>
2024-02-13 11:51:25 +0100

    fix: SSD my implementation

--- openood/postprocessors/ssd_postprocessor.py
+++ openood/postprocessors/ssd_postprocessor.py
@@ -1,24 +1,89 @@
+from copy import deepcopy
+from typing import Any
+
+import faiss
+import numpy as np
+import sklearn.covariance
 import torch
 import torch.nn as nn
+from tqdm import tqdm
+
 from .base_postprocessor import BasePostprocessor
-from .mds_ensemble_postprocessor import get_MDS_stat
+from .info import num_classes_dict
 
 
-class SSDPostprocessor(BasePostprocessor):
+class UnsupervisedMDSPostprocessor(BasePostprocessor):
     def __init__(self, config):
         self.config = config
-        self.postprocessor_args = config.postprocessor.postprocessor_args
-
-        self.feature_type_list = self.postprocessor_args.feature_type_list
-        self.reduce_dim_list = self.postprocessor_args.reduce_dim_list
-
-        # self.num_classes = self.config.dataset.num_classes
         self.num_classes = 1
-        self.num_layer = len(self.feature_type_list)
-
-        self.feature_mean, self.feature_prec = None, None
+        self.setup_flag = False
 
     def setup(self, net: nn.Module, id_loader_dict, ood_loader_dict):
-        self.feature_mean, self.feature_prec, self.transform_matrix = \
-            get_MDS_stat(net, id_loader_dict['train'], self.num_classes,
-                         self.feature_type_list, self.reduce_dim_list)
+        if not self.setup_flag:
+            # estimate mean and variance from training set
+            print("\n Estimating mean and variance from training set...")
+            all_feats = []
+            all_labels = []
+            all_preds = []
+            with torch.no_grad():
+                for batch in tqdm(
+                    id_loader_dict["train"], desc="Setup: ", position=0, leave=True
+                ):
+                    data, labels = batch["data"].cuda(), batch["label"]
+                    logits, features = net(data, return_feature=True)
+                    all_feats.append(features.cpu())
+                    all_labels.append(deepcopy(labels))
+                    all_preds.append(logits.argmax(1).cpu())
+
+            all_feats = torch.cat(all_feats)
+            all_labels = torch.cat(all_labels)
+            all_preds = torch.cat(all_preds)
+            # sanity check on train acc
+            train_acc = all_preds.eq(all_labels).float().mean()
+            print(f" Train acc: {train_acc:.2%}")
+
+            # compute clusters
+            all_clusters = self.compute_clusters(all_feats)
+
+            # compute class-conditional statistics
+            self.class_mean = []
+            centered_data = []
+            for c in range(self.num_classes):
+                class_samples = all_feats[all_clusters.eq(c)].data
+                self.class_mean.append(class_samples.mean(0))
+                centered_data.append(class_samples - self.class_mean[c].view(1, -1))
+
+            self.class_mean = torch.stack(
+                self.class_mean
+            )  # shape [#classes, feature dim]
+
+            group_lasso = sklearn.covariance.EmpiricalCovariance(assume_centered=False)
+            group_lasso.fit(torch.cat(centered_data).cpu().numpy().astype(np.float32))
+            # inverse of covariance
+            self.precision = torch.from_numpy(group_lasso.precision_).float()
+            self.setup_flag = True
+        else:
+            pass
+
+    @torch.no_grad()
+    def postprocess(self, net: nn.Module, data: Any):
+        logits, features = net(data, return_feature=True)
+        pred = logits.argmax(1)
+
+        class_scores = torch.zeros((logits.shape[0], self.num_classes))
+        for c in range(self.num_classes):
+            tensor = features.cpu() - self.class_mean[c].view(1, -1)
+            class_scores[:, c] = -torch.matmul(
+                torch.matmul(tensor, self.precision), tensor.t()
+            ).diag()
+
+        conf = torch.max(class_scores, dim=1)[0]
+        return pred, conf
+
+    def compute_clusters(self, features: torch.Tensor):
+        kmeans = faiss.Kmeans(
+            features.shape[1], self.num_classes, niter=100, verbose=False, gpu=False
+        )
+        kmeans.train(np.random.permutation(features.numpy()))
+        _, ypred = kmeans.assign(features.numpy())
+        return torch.tensor(ypred)
